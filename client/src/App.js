7d26f200d72d99f89e9e6f092fe61b3b87c5556c
wassimmm <wassim.hmouda@esprit.tn>
2023-03-30 09:25:11 +0200

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import AddServer from "./components/dashboard/AddServer.js";
 import Server from "./components/dashboard/Server.js";
 import CpuServer from "./components/dashboard/Cpuserver.js";
+import Networkserver from "./components/dashboard/Networkserver.js";
 import "./App.css";
 
 // Check for token to keep user logged in
@@ -51,6 +52,7 @@ class App extends Component {
               <PrivateRoute exact path="/addServer" component={AddServer} />
               <PrivateRoute exact path="/server/:id" component={Server} />
               <PrivateRoute exact path="/cpu/:id" component ={CpuServer}/>
+              <PrivateRoute exact path="/network/:id" component ={Networkserver}/>
             
             </Switch>
               
---
+++ client/src/components/component/networkTable.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import  '../styles/process_table.css'
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {connectToServer} from "../../actions/serverActions.js";
+
+// let onKillClick = (e,pid, serverId, userId, connectToServer) => {
+//     e.preventDefault();
+//     connectToServer(serverId, userId, "sudo kill -9 "+pid);
+// };
+
+function networkTable(props) {
+    const { data, serverId } = props;
+
+    // Check if the data prop is defined
+    if (!data) {
+        return null;
+    }
+
+    const { user } = props.auth;
+
+    return (
+        <table className="table-network">
+            <thead className="thead-network">
+            <tr className="tr-network">
+                <th className="th-network">device</th>
+                <th className="th-network">type</th>
+                <th className="th-network">state</th>
+                <th className="th-network">connection</th>
+               
+            </tr>
+            </thead>
+            <tbody className="tbody-network">
+            {data.map((item) => (
+                <tr className="tr-network" key={item.pid}>
+                    <td className="td-network">{item.device}</td>
+                    <td className="td-network">{item.type}</td>
+                    <td className="td-network">{item.state}</td>
+                    <td className="td-network">{item.connection}</td>
+                    
+                    <td
+                        className="td-network"
+                        style={{ display: "flex", justifyContent: "space-around" }}
+                    >
+                        {/* <button onClick={(e) => onKillClick(e, item.pid, serverId, user.id, props.connectToServer)}>KILL</button>
+                        <button>DETAILS</button> */}
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+}
+
+networkTable.propTypes = {
+    status: PropTypes.object.isRequired,
+    connectToServer: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    serverError: PropTypes.string.isRequired,
+    serverId: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    serverError: state.serverError,
+    status: state.server
+});
+
+export default connect(mapStateToProps, { connectToServer })(networkTable);
